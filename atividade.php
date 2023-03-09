5b20006ed6f33b949f813b497ca9b603aa2704cb
LETICIA ARAUJO PEREIRA <leticia.1970@aluno.pr.senac.br>
2023-03-09 11:33:00 -0300

    mensagem

---
+++ atividade.php
@@ -0,0 +1,14 @@
+<?php
+    $a = "10";
+    $b = 1000;
+    $c = 100;
+    $d = 10;
+
+if (($a == $d) and ($c > $d)){
+    echo "As informçãoes são positivas";
+}
+
+else {
+    echo "As informações são negativas";
+}
+?>
---
+++ teste.php
@@ -0,0 +1,9 @@
+<?php
+$numero = 101;
+
+if ($numero % 2 == 0)
+    echo "é par";
+else 
+ echo "é impar";
+
+ ?>
